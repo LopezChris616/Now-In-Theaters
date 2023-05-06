b6c7152a82f7d3991062d69686c804089c33a2f9
Christian Lopez <lopezchristian1610@gmail.com>
2023-05-06 19:41:34 -0400

    Click a button to reveal upcoming movie releases

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
 import { key } from "./key.js";
 
-function getNowPlaying() {
-    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&region=US&page=1`)
-        .then(res => res.json())
-        .then(movies => movies.results.forEach(movie => getMovieInfo(movie)))
-        .catch(err => console.log(err))
+function init() {
+    const movieDisplay = document.getElementById("movie-display");
+    getNowPlaying(movieDisplay);
+    getUpcoming(movieDisplay)
 }
 
-getNowPlaying();
+init();
 
-function getMovieInfo(movie) {
-    const nowPlaying = document.getElementById("now-playing");
+function getNowPlaying(movieDisplay) {
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&region=US&page=1`)
+        .then(res => res.json())
+        .then(movies => movies.results.forEach(movie => getMovieInfo(movie, movieDisplay)))
+        .catch(err => console.log(err));
+}
+
+function getUpcoming(movieDisplay) {
+    const viewToggle = document.getElementById("view-toggle");
+    viewToggle.addEventListener("click", () => {
+        fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&region=US&page=1`)
+        .then(res => res.json())
+        .then(movies => {
+            movieDisplay.textContent = "";
+            movies.results.forEach(movie => getMovieInfo(movie, movieDisplay));
+        })
+        .catch(err => console.log(err));
+    });
+}
+
+function getMovieInfo(movie, movieDisplay) {
     const movieCard = document.createElement("div");
     const movieTitle = document.createElement("h3");
     const movieTickets = document.createElement("a");
@@ -19,8 +37,8 @@ function getMovieInfo(movie) {
     const movieReleaseDate = document.createElement("p");
 
     movieCard.setAttribute("class", "movie-card");
-    movieTitle.textContent = movie.original_title;
-    movieTickets.setAttribute("href", `https://www.fandango.com/search?q=${modifyTitle(movie.original_title)}&mode=all`);
+    movieTitle.textContent = movie.title;
+    movieTickets.setAttribute("href", `https://www.fandango.com/search?q=${modifyTitle(movie.title)}&mode=all`);
     movieTickets.setAttribute("target", "_blank");
     movieTickets.textContent = "Get tickets here!";
     moviePoster.setAttribute("src", `https://image.tmdb.org/t/p/original${movie.poster_path}`);
@@ -28,8 +46,12 @@ function getMovieInfo(movie) {
     movieReleaseDate.textContent = movie.release_date;
 
     movieCard.append(movieTitle, moviePoster, movieReleaseDate, movieRating, movieTickets);
-    nowPlaying.appendChild(movieCard);
+    movieDisplay.appendChild(movieCard);
+
+    console.log(movie);
 }
+
+
 
 function modifyTitle(title) {
     const newTitle = title.split("").map(letter => {
