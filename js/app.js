d762459e90e2c9bbcd2cd87d7cca7b61bba16c36
Christian Lopez <lopezchristian1610@gmail.com>
2023-05-06 17:54:04 -0400

    Add link to Fandango that searches for movie to buy tickets

--- js/app.js
+++ js/app.js
@@ -13,18 +13,30 @@ function getMovieInfo(movie) {
     const nowPlaying = document.getElementById("now-playing");
     const movieCard = document.createElement("div");
     const movieTitle = document.createElement("h3");
-    const movieOverview = document.createElement("p");
+    const movieTickets = document.createElement("a");
     const moviePoster = document.createElement("img");
     const movieRating = document.createElement("p");
     const movieReleaseDate = document.createElement("p");
 
     movieCard.setAttribute("class", "movie-card");
     movieTitle.textContent = movie.original_title;
-    movieOverview.textContent = movie.overview;
+    movieTickets.setAttribute("href", `https://www.fandango.com/search?q=${modifyTitle(movie.original_title)}&mode=all`);
+    movieTickets.setAttribute("target", "_blank");
+    movieTickets.textContent = "Get tickets here!";
     moviePoster.setAttribute("src", `https://image.tmdb.org/t/p/original${movie.poster_path}`);
     movieRating.textContent = `${movie.vote_average}/10 | ${movie.vote_count} ratings`;
     movieReleaseDate.textContent = movie.release_date;
 
-    movieCard.append(movieTitle, moviePoster, movieReleaseDate, movieRating, movieOverview);
+    movieCard.append(movieTitle, moviePoster, movieReleaseDate, movieRating, movieTickets);
     nowPlaying.appendChild(movieCard);
+}
+
+function modifyTitle(title) {
+    const newTitle = title.split("").map(letter => {
+        if(letter === " ") {
+            letter = "+";
+        }
+        return letter;
+    })
+    return newTitle.join("");
 }
