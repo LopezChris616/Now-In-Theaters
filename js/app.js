301e3d23ee581e528053cf8c0975ccbce8f7fb1a
Christian Lopez <lopezchristian1610@gmail.com>
2023-05-06 05:42:52 -0400

    Get list of movies now playing in theaters

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 import { key } from "./key.js";
 
+function getNowPlaying() {
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
+        .then(res => res.json())
+        .then(films => console.log(films))
+        .catch(err => console.log(err))
+}
+
+getNowPlaying();
